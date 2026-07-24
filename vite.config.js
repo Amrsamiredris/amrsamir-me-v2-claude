c8dbd7094c2287f9d763515f8f0d441784319c57
Amrsamiredris <32356809+Amrsamiredris@users.noreply.github.com>
2026-07-24 20:19:18 +0400

    feat: restructure to multi-page app for subdomains

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   root: '.',
@@ -7,7 +8,12 @@ export default defineConfig({
     outDir: 'dist',
     assetsInlineLimit: 0,
     rollupOptions: {
-      input: './index.html',
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        events: resolve(__dirname, 'events/index.html'),
+        marketing: resolve(__dirname, 'marketing/index.html'),
+        ai: resolve(__dirname, 'ai/index.html'),
+      },
     },
   },
   server: {
